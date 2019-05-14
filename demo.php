dfdcebdca3a56f2e8a05fef19bad2f082c66b2d9
lihaijiang <lihaijiang@bianfeng.com>
2019-05-14 11:17:55 +0800

    first commit

---
+++ demo.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lihaijiang
+ * Date: 2019/5/14
+ * Time: 11:14
+ */
+require_once './vendor/autoload.php';
+
+use \Jiang\Smile\TestController;
+
+TestController::echo();
---
+++ src/smile/TestController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lihaijiang
+ * Date: 2019/5/14
+ * Time: 11:10
+ */
+
+namespace Jiang\Smile;
+
+class TestController
+{
+    public function __construct()
+    {
+    }
+
+    public static function echo()
+    {
+        echo 'Hello World';
+    }
+}
